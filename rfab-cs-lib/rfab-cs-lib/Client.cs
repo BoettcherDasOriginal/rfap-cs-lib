e56351a95a684e291abc159cb8c83facc2af2f23
BoettcherDasOriginal <boettcherleonard@gmail.com>
2021-11-27 17:06:22 +0100

    Client.cs added

---
+++ rfab-cs-lib/rfab-cs-lib/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace rfab_cs_lib
+{
+    public class Client
+    {
+        TcpClient tcpClient = null;
+
+        public bool Connect(IPAddress iPAddress, int port)
+        {
+            tcpClient = new TcpClient();
+            tcpClient.Connect(iPAddress, port);
+            return tcpClient.Connected;
+        }
+    }
+}
