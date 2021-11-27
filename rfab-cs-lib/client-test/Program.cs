8f2d711697fa26f0811910aedf0df52e704d9677
BoettcherDasOriginal <boettcherleonard@gmail.com>
2021-11-27 21:50:58 +0100

    Update

---
+++ rfab-cs-lib/client-test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using rfab_cs_lib;
+using System.Net;
+
+namespace client_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.Connect(IPAddress.Parse("127.0.0.1"), 6700);
+
+
+            Console.ReadLine();
+        }
+    }
+}
